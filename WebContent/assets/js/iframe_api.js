7fd1a007372b26799eb4ed6250c11d9537aba27b
hellochaeyoung <cyahn0831@hansung.ac.kr>
2022-07-12 15:58:37 +0900

    style: [AllFiles] 파일 진행 이력 주석 추가 #58

--- WebContent/assets/js/iframe_api.js
+++ WebContent/assets/js/iframe_api.js
@@ -1,2 +1,12 @@
+/**
+ * [프로젝트]롯데e커머스_자바전문가과정
+ * [시스템명]영화예매시스템
+ * [팀 명]CUTEam
+ * [파일명]iframe_api.js
+ * -----------------------------------------------------------
+ * 수정일자           수정자         수정내용
+ * 2022.07.08       이성은         신규생성
+ * -----------------------------------------------------------
+ */
 
 if (!window['YT']) {var YT = {loading: 0,loaded: 0};}if (!window['YTConfig']) {var YTConfig = {'host': 'http://www.youtube.com'};}if (!YT.loading) {YT.loading = 1;(function(){var l = [];YT.ready = function(f) {if (YT.loaded) {f();} else {l.push(f);}};window.onYTReady = function() {YT.loaded = 1;for (var i = 0; i < l.length; i++) {try {l[i]();} catch (e) {}}};YT.setConfig = function(c) {for (var k in c) {if (c.hasOwnProperty(k)) {YTConfig[k] = c[k];}}};var a = document.createElement('script');a.type = 'text/javascript';a.id = 'www-widgetapi-script';a.src = 'https://s.ytimg.com/yts/jsbin/www-widgetapi-vflTZdOF2/www-widgetapi.js';a.async = true;var c = document.currentScript;if (c) {var n = c.nonce || c.getAttribute('nonce');if (n) {a.setAttribute('nonce', n);}}var b = document.getElementsByTagName('script')[0];b.parentNode.insertBefore(a, b);})();}
--- src/controller/LocalDateTimeSerializer.java
+++ src/controller/LocalDateTimeSerializer.java
@@ -11,16 +11,13 @@ import java.time.format.DateTimeFormatter;
 
 /**
  * [프로젝트]롯데e커머스_자바전문가과정
- *
  * [시스템명]영화예매시스템
- *
- * [팀 명]CUTEam -----------------------------------------------------------
- *
- * 수정일자 수정자 수정내용
- *
- * 2022.07.12 권나연 신규생성, GSON private final field 생성 오류 처리 클래스
- *
- * -----------------------------------------------------------
+ * [팀 명]CUTEam
+ * [파일명]LocalDateTimeSerializer.java
+ * ---------------------------------------------------------------------------------------------
+ * 수정일자             수정자             수정내용
+ * 2022.07.12         권나연              신규생성, GSON private final field 생성 오류 처리 클래스
+ * ----------------------------------------------------------------------------------------------
  */
 public class LocalDateTimeSerializer implements JsonSerializer<LocalDateTime> {
     private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
